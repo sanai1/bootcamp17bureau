273852f5e2c7439cb2c79499196ee298261a29b5
Klaymerr <91547003+Klaymerr@users.noreply.github.com>
2025-03-14 19:20:36 +0300

    Update txtmarkdown.py

--- txtmarkdown.py
+++ txtmarkdown.py
@@ -7,7 +7,7 @@ from secretsData import YANDEX_API_KEY,YANDEX_FOLDER_ID,gpt_model
 folder_id = YANDEX_FOLDER_ID
 api_key = YANDEX_API_KEY
 
-def textToSum(user_prompt) -> str:
+def txtmarkdown(user_prompt) -> str:
     system_prompt = "Copy---title:- AI Playgroundauthor:- Ashwin Kumar Karnadtheme:- Copenhagendate:- March 22, 2020--- # What's AI?- Artificial Intelligence- The ability of machine to think and behave like humans.- How does the machine learn on its own? - That is called Machine Learning. ML is the study of computer algorithms that improve automatically with experience.- Just like humans learn with experience - Machines also learn with experience!- Examples of common AI? Alexa, Siri, Google Home, Self Driving Cars, Robots etc.# What's out there?![Verticles](img/Untitled.png)# How do computers make decisions?- Conditional statements are used to perform different actions based on different conditions.- In many programming languages, decisions (also called conditionals) take the form of an if-then construct. They start with a condition, which is then evaluated as either True or False.# How do computers make decisions?![Flow chart](img/Untitled 1.png){ width=250px } Сделай конспетк в этом стиле"
     body = {
         'modelUri': f'gpt://{folder_id}/{gpt_model}',
