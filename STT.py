ad69f16e63cef427409aad78763890099d2e606f
Klaymerr <91547003+Klaymerr@users.noreply.github.com>
2025-03-14 17:21:41 +0300

    add local save support to STT

--- STT.py
+++ STT.py
@@ -3,13 +3,14 @@ from aiogram.types import FSInputFile
 
 from secretsData import stt_token_bot, folder_id
 
-def recognize_speech(audio_file, lang='ru-RU'):
+def recognize_speech(file_name, lang='ru-RU'):
     url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={folder_id}&lang={lang}"
     headers = {
         "Authorization": f"Api-Key {stt_token_bot}"
     }
 
-    response = requests.post(url, headers=headers, data=audio_file)
+    with open('docs/audio' + file_name + '.odd', "rb") as audio_file:
+        response = requests.post(url, headers=headers, data=audio_file)
 
     if response.status_code == 200:
         return response.json().get("result", "Ошибка: не удалось распознать речь")
