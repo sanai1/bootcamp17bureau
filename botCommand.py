75f234d57f4df3126b99c2f0698e62dbf80c8f33
aleksandr <sanek.v.07@gmail.com>
2025-03-15 00:54:55 +0300

    add video and audio function

--- botCommand.py
+++ botCommand.py
@@ -5,6 +5,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, FSInputFile
 from aiogram.utils.chat_action import ChatActionMiddleware
 from moviepy import VideoFileClip
+from pydub import AudioSegment
 
 import secretsData
 from STT import recognize_speech
@@ -50,25 +51,25 @@ async def video(message: Message):
 
 @router.message(lambda message: message.video is not None)
 async def handler_video(message: types.Message):
-    user_id = message.from_user.id
-    # video_ = message.video
-    # file_id = video_.file_id
-    #
-    # file = await bot.get_file(file_id)
-    # file_path = file.file_path
-    # await bot.download_file(file_path, f"{user_id}_video.mp4")
-    #
-    # video_clip = VideoFileClip(f"{user_id}_video.mp4")
-    # audio_clip = video_clip.audio
-    #
-    # audio_clip.write_audiofile(f"{user_id}.ogg", codec='libvorbis')
-    #
-    # ogg_audio_file = FSInputFile(f"{user_id}.ogg")
-    #
-    # await message.answer_audio(ogg_audio_file)
-    #
-    # os.remove(f"{user_id}_video.mp4")
-    # os.remove(f"{user_id}.ogg")
+    user_id = str(message.from_user.id)
+    video_ = message.video
+    file_id = video_.file_id
+
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    await bot.download_file(file_path, f"{user_id}.mp4")
+
+    video_clip = VideoFileClip(f"{user_id}.mp4")
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(f"{user_id}.mp3")
+
+    audio_segment = AudioSegment.from_mp3(f"{user_id}.mp3")
+    audio_segment.export(f"docs/audio/{user_id}.ogg", format="ogg")
+
+    await print_info(message, user_id)
+
+    os.remove(f"{user_id}.mp4")
+    os.remove(f"{user_id}.mp3")
 
 
 @router.message(lambda message: message.voice is not None)
@@ -81,40 +82,39 @@ async def handler_voice(message: types.Message):
     user_id = str(message.from_user.id)
     await bot.download_file(file_path, f"docs/audio/{user_id}.ogg")
 
-    text = recognize_speech(user_id)
-    print(text == "")
-    text_sum = text_to_sum(text)
-    await message.answer(f"Конспект по теме:\n\n{text_sum}")
-    test = getTest(text)
-    await message.answer(f"Тесты по конспекту:\n\n{test}")
-    markdown = txt_markdown(text)
-    await message.answer(f"Формат markdown для создания презентации:\nСервисы для конвертации MD в PFD\nhttps://apitemplate.io/pdf-tools/convert-markdown-to-pdf/\n\nhttps://www.markdowntopdf.com/")
-    await message.answer(markdown)
-    # f = open(f"docs/presentation/{user_id}.txt", "w")
-    # f.write(markdown)
-    # convert_presentation(f"docs/presentation/{user_id}.txt", output_format="pdf")
-
+    await print_info(message, user_id)
 
 
 @router.message(lambda message: message.audio is not None)
 async def handler_audio(message: types.Message):
     user_id = str(message.from_user.id)
-    # audio_ = message.audio
-    # file_id = audio_.file_id
-    #
-    # file = await bot.get_file(file_id)
-    # file_path = file.file_path
-    # await bot.download_file(file_path, "audio_file.mp3")
-    #
-    # audio_file = FSInputFile("audio_file.mp3")
-    # await message.reply_audio(audio_file)
-    #
-    # os.remove("audio_file.mp3")
+    audio_ = message.audio
+    file_id = audio_.file_id
+
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    await bot.download_file(file_path, f"{user_id}.mp3")
+
+    audio_segment = AudioSegment.from_mp3(f"{user_id}.mp3")
+    audio_segment.export(f"docs/audio/{user_id}.ogg", format="ogg")
+
+    await print_info(message, user_id)
+
+    os.remove(f"{user_id}.mp3")
 
 
+async def print_info(message: types.Message, user_id: str):
+    text = recognize_speech(user_id)
+    print(text == "")
+    text_sum = text_to_sum(text)
+    await message.answer(f"Конспект по теме:\n\n{text_sum}")
 
+    tests = getTest(text_sum)
+    await message.answer(f"Тесты по конспекту:\n\n{tests}")
 
-
+    markdown = txt_markdown(text_sum)
+    await message.answer(f"Формат markdown для создания презентации:\nСервисы для конвертации MD в PFD\nhttps://apitemplate.io/pdf-tools/convert-markdown-to-pdf/\n\nhttps://www.markdowntopdf.com/")
+    await message.answer(markdown)
 
 
 
