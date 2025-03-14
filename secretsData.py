af3f8d80e403ebebd2e0619623e311bc1d988d7e
Klaymerr <91547003+Klaymerr@users.noreply.github.com>
2025-03-14 16:53:40 +0300

    deleted trash

--- secretsData.py
+++
@@ -1,4 +0,0 @@
-token_bot = "7943670295:AAHEYh-EERtn5vmTS7Z-yN7-i2oIsCSZRCU"
-folder_id = "b1gug7c74crq38i2spt2"
-api_key = "AQVN1yb531bBWXGKC-nQY-I_7JsnIyPl-lXPGsC1"
-stt_token_bot = 'AQVNyXc5dzEN9LpX6QFtW0YBm-kv78cc-iGl4zdS'
